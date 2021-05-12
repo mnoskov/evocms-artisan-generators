ce1bbd063ba7711e9732579b67ae302c03003552
windx-foobar <bezalkogoln1ycoder@gmail.com>
2021-05-12 23:32:05 +0300

    added middleware command to service provider

--- src/ArtisanGeneratorsServiceProvider.php
+++ src/ArtisanGeneratorsServiceProvider.php
@@ -5,15 +5,14 @@ namespace EvolutionCMS\ArtisanGenerators;
 use EvolutionCMS\ArtisanGenerators\Console;
 use Illuminate\Support\ServiceProvider;
 
-class ArtisanGeneratorsServiceProvider extends ServiceProvider
-{
-    public function register()
-    {
-        $this->commands([
-            Console\ConsoleMakeCommand::class,
-            Console\ControllerMakeCommand::class,
-            Console\ModelMakeCommand::class,
-            Console\SeederMakeCommand::class,
-        ]);
-    }
+class ArtisanGeneratorsServiceProvider extends ServiceProvider {
+   public function register() {
+      $this->commands([
+         Console\ConsoleMakeCommand::class,
+         Console\ControllerMakeCommand::class,
+         Console\ModelMakeCommand::class,
+         Console\SeederMakeCommand::class,
+         Console\MiddlewareMakeCommand::class
+      ]);
+   }
 }
